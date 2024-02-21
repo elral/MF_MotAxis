757f7091dfe48a8375b1a669999de4cda1fda0e6
elral <3263285+elral@users.noreply.github.com>
2024-02-21 10:19:17 +0100

    add fixed config

--- MotAxis/MFCustomDevicesConfig.h
+++ MotAxis/MFCustomDevicesConfig.h
@@ -3,16 +3,17 @@
 #include <Arduino.h>
 
 // If no input custom deivece is defined, uncomment the following line
-const char CustomDeviceConfig[] PROGMEM = {""};
+// const char CustomDeviceConfig[] PROGMEM = {""};
 
-/*
 // Otherwise define your input custom devices
 const char CustomDeviceConfig[] PROGMEM = 
 {
-    "1.2.Button Flash:"
-    "8.3.4.0.Encoder Flash:"
-    "11.54.5.Analog Input Flash:"
-    "12.7.6.5.1.InputShifter Flash:"
-    "14.12.8.9.10.11.2.Multiplexer Flash:"
+    "3.4.TrimEnable:"
+    "3.7.ThrottleEnable:"
+    "1.10.SyncLostTrim:"
+    "1.11.SyncLostThrottle:"
+    "11.14.5.TrimWheel:"
+    "11.15.5.Throttle:"
+    "15.2.3.5.6.0.2.0.0.3.Stepper TrimWheel:"
+    "17.ELRAL_MOTAXIS.21..Elral's Mot Axis:"
 };
-*/
